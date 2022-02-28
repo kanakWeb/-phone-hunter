39fe177d53c284e4b19dacd7a56668358ff34bbb
kanakweb <kanakwebdev@gmail.com>
2022-03-01 03:31:52 +0600

    added details info

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // https://openapi.programming-hero.com/api/phones?search
 const search=()=>{
+    
     const searchSield=document.getElementById('search-field').value
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchSield}`
     fetch(url)
@@ -17,18 +18,41 @@ phones.forEach((phone)=> {
     <h5 class="font-size">Brand:${phone.brand}</h5>
     <div class="allButton">
     <button type="button" class="btn  btn-secondary">Grey</button>
-    <button type="button" class="btn btn-success ">Green</button>
+    <button onclick="details('${phone.slug}')" id="details-id" data-bs-toggle="modal" data-bs-target="#staticBackdrop" type="button" class="btn btn-success ">Details</button>
 </div>
 </div>`
 
 phoneInfo.appendChild(div)
 
 });
+
 }
+const details=(phoneID)=>{
+    
+    
+    const url=`https://openapi.programming-hero.com/api/phone/${phoneID}`
+   fetch(url)
+   .then(Response=>Response.json())
+   .then(json=>getDetails(json.data))
+}
+ const getDetails=(detailsID)=>{
+   const detailsCard=document.getElementById('details-card')
+   const div=document.createElement('div')
+   div.innerHTML=`<div class="text-center">
+   <div class="card p-4" style="width:400px">
+       <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+       <div class="card-body">
+       <div  class="text-center"><img width="100px" class="img-fluid" src="${phone.image}" alt="phone"></div>
+           <p class="card-text">Storage:${detailsID.mainFeatures.storage}</p>
+           <p class="card-text">DisplaySize:</p>
+           <p class="card-text">ChipSet:</p>
+           <p class="card-text">Memory:</p>
+           <p class="card-text">Ssensors:</p>
 
+       </div>
+   </div>
+</div>`
+detailsCard.appendChild(div)
 
-{/* <div class="card border-warning phoneInformation">
-                    <img src="" alt="phone">
-                    <h4>Name:</h4>
-                    <h5>Brand:</h5>
-                </div> */}
+ }
+ 
