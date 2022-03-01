774c46547d8257880be4212ac2be3c2895d71b2b
kanakweb <kanakwebdev@gmail.com>
2022-03-01 15:48:02 +0600

    releasedate handle

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-// https://openapi.programming-hero.com/api/phones?search
+// https://openapi.programming-hero.com/api/phones?search //api
 const search=()=>{
+    
     document.getElementById('phone-Info').innerText=' '
     const searchField=document.getElementById('search-field').value;
     const url=`https://openapi.programming-hero.com/api/phones?search=${searchField}`
@@ -7,7 +8,11 @@ const search=()=>{
     .then(Response=>Response.json())
     .then(json=>displayPhone(json.data))
 }
+
+
+
 const displayPhone=(phones)=>{
+    document.getElementById('details-card').innerHTML=' '
     const phoneInfo=document.getElementById('phone-Info')
 
     const phoneDataOneToTen=phones.slice(0,20)
@@ -41,23 +46,51 @@ const details=(phoneID)=>{
    .then(Response=>Response.json())
    .then(json=>displayDetails(json.data))
 }
+
+
  const displayDetails=(detailsID)=>{
-   const detailsCard=document.getElementById('details-card')
-   const div=document.createElement('div')
-   div.innerHTML=`<div class="text-center">
-   <div class="card p-4" style="width:400px">
-   <div  class="text-center"><img width="100px" class="img-fluid" src="${detailsID.image}" alt="phone"></div>
-       <div class="card-body">
-       <h5>Name: ${detailsID.name}</h5>
-           <p class="card-text">Storage:${detailsID.mainFeatures.storage}</p>
-           <p class="card-text">DisplaySize:${detailsID.mainFeatures.displaySize}</p>
-           <p class="card-text">ChipSet:${detailsID.mainFeatures.ChipSet}</p>
-           <p class="card-text">Memory:${detailsID.mainFeatures.memory}</p>
-           <p class="card-text">Sensors:${detailsID.mainFeatures.sensors}</p>
-       </div>
-   </div>
-</div>`
-detailsCard.appendChild(div)
+    
+  if(detailsID.releaseDate==''){
+    const detailsCard=document.getElementById('details-card')
+    const div=document.createElement('div')
+    div.innerHTML=`<div class="text-center">
+    <div class="card p-4" style="width:400px">
+    <div  class="text-center"><img width="100px" class="img-fluid" src="${detailsID.image}" alt="phone"></div>
+        <div class="card-body">
+        <h5>ReleaseDate: No Release</h5>
+        <h5>Name: ${detailsID.name}</h5>
+            <p class="card-text">Storage:${detailsID.mainFeatures.storage}</p>
+            <p class="card-text">DisplaySize:${detailsID.mainFeatures.displaySize}</p>
+            <p class="card-text">ChipSet:${detailsID.mainFeatures.ChipSet}</p>
+            <p class="card-text">Memory:${detailsID.mainFeatures.memory}</p>
+            <p class="card-text">Sensors:${detailsID.mainFeatures.sensors}</p>
+        </div>
+    </div>
+ </div>`
+ detailsCard.appendChild(div)
+  }
+
+
+
+  else{
+    const detailsCard=document.getElementById('details-card')
+    const div=document.createElement('div')
+    div.innerHTML=`<div class="text-center">
+    <div class="card p-4" style="width:400px">
+    <div  class="text-center"><img width="100px" class="img-fluid" src="${detailsID.image}" alt="phone"></div>
+        <div class="card-body">
+        <h5>ReleaseDate: ${detailsID.releaseDate}</h5>
+        <h5>Name: ${detailsID.name}</h5>
+            <p class="card-text">Storage:${detailsID.mainFeatures.storage}</p>
+            <p class="card-text">DisplaySize:${detailsID.mainFeatures.displaySize}</p>
+            <p class="card-text">ChipSet:${detailsID.mainFeatures.ChipSet}</p>
+            <p class="card-text">Memory:${detailsID.mainFeatures.memory}</p>
+            <p class="card-text">Sensors:${detailsID.mainFeatures.sensors}</p>
+        </div>
+    </div>
+ </div>`
+ detailsCard.appendChild(div)
+  }
 
  }
  
